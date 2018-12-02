e550cc2408e237c98519b90873562c233ee3c8d0
lhirondellereviendra <lhirondellevoyage@gmail.com>
2018-12-02 16:52:41 +0100

    correction d'une erreur

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,5 +4,4 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
