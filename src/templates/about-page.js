7adbfd4742b456aa1fb0b5adc5b0e04caa32d5df
lhirondellereviendra <lhirondellevoyage@gmail.com>
2018-12-08 11:36:42 +0100

    about modif

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -10,20 +10,20 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
+          <section className="section section--gradient">
+            <div className="container">
+              <div className="columns">
+                <div className="column is-10 is-offset-1">
+                  <div className="section">
+                    <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                      {title}
+                    </h2>
+                    <PageContent className="content" content={content} />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+          </section>
   )
 }
 
@@ -37,14 +37,14 @@ const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
-    <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
-    </Layout>
-    <Footer/>
+        <Layout>
+          <AboutPageTemplate
+            contentComponent={HTMLContent}
+            title={post.frontmatter.title}
+            content={post.html}
+          />
+        </Layout>
+        <Footer/>
   )
 }
 
