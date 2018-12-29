ff99a7415f341436bac535c7a06b0bd7b8d12d91
lhirondellereviendra <lhirondellevoyage@gmail.com>
2018-12-29 12:19:06 +0100

    modif CMS

---
+++ src/cms/preview-templates/HomePreview.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { HomePage } from '../../templates/home-page'
+
+const HomePreview = ({ entry, getAsset }) => {
+
+  const entryCategorie= entry.getIn(['data', 'Categorie'])
+  const Categorie = entryCategorie ? entryCategorie.toJS() : []
+
+  return (
+    <HomePage
+      image={entry.getIn(['data', 'image'])}
+      description={entry.getIn(['data', 'description'])}
+      about={{
+        titre: entry.getIn(['data', 'about', 'titre']),
+        description: entry.getIn(['data', 'about', 'description']),
+        image: entry.getIn(['data', 'about', 'image']),
+      }}
+      Categorie={Categorie}
+    />
+  )
+}
+
+HomePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default HomePreview
