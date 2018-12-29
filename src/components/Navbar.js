8a8bb4f0117b1f3011fe269353c1ed9a81df9e0a
lhirondellereviendra <lhirondellevoyage@gmail.com>
2018-12-30 00:20:17 +0100

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,7 @@
 import React from 'react' /* eslint-disable */
 import logo from '../img/logo.svg'
+import './menumobile.js'
 
-
-if (typeof window != 'undefined'){
-        window.addEventListener('DOMContentLoaded', () => {
-  
-            // Get all "navbar-burger" elements
-            const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-          
-            // Check if there are any navbar burgers
-            if ($navbarBurgers.length > 0) {
-          
-              // Add a click event on each of them
-              $navbarBurgers.forEach( el => {
-                el.addEventListener('click', () => {
-          
-                  // Get the target from the "data-target" attribute
-                  const target = el.dataset.target;
-                  const $target = document.getElementById(target);
-          
-                  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                  el.classList.toggle('is-active');
-                  $target.classList.toggle('is-active');
-          
-                });
-              });
-            }
-          
-          }); 
-    } 
 
 const Navbar = () => (
 <div>
@@ -59,13 +32,15 @@ const Navbar = () => (
 
           </div>
           
-          <div className="navbar-end navbar-menu" id="navMenu">
+          <div className="navbar-menu" id="navMenu">
+                <div className="navbar-end"> 
+
                             <a className="navbar-item is-size-5 " aria-hidden="true" href="https://lhirondellereviendra.com/apropos">Me connaitre </a> 
                             <p className="navbar-item-trait"> | </p>                            
                             <a className="navbar-item is-size-5 " aria-hidden="true" href="https://lhirondellereviendra.com/blog" >Mon Blog </a>
                             <p className="navbar-item-trait"> | </p>                            
                             <a className="navbar-item is-size-5 " aria-hidden="true" href="https://lhirondellereviendra.com/#contact">Me suivre </a>
-
+                </div>
           </div>
                     <a href="/">                                           
                             <figure className="image" style={{maxWidth:"900px",minWidth:"200px",margin:"auto"}}>
