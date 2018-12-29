d99fc70748b25353353b66ee06954a8acbdb27c5
lhirondellereviendra <lhirondellevoyage@gmail.com>
2018-12-29 14:41:48 +0100

    add

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,9 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import HomePreview from './preview-templates/HomePreview'
+//import HomePreview from './preview-templates/HomePreview'
 
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('home', HomePreview)
+//CMS.registerPreviewTemplate('home', HomePreview)
