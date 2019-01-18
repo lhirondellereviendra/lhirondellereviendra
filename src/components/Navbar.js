372eafb6454278ae015a57ea3205cc761fbc39dd
Alexis Lepresle <33030155+alexislepresle@users.noreply.github.com>
2019-01-19 00:34:18 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,33 @@
 import React from 'react' /* eslint-disable */
 import logo from '../img/logo.svg'
 
-const Navbar = () => (
+const Navbar = class extends React.Component {
+
+  componentDidMount() {
+    // Get all "navbar-burger" elements
+   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+   if ($navbarBurgers.length > 0) {
+ 
+     // Add a click event on each of them
+     $navbarBurgers.forEach( el => {
+       el.addEventListener('click', () => {
+ 
+         // Get the target from the "data-target" attribute
+         const target = el.dataset.target;
+         const $target = document.getElementById(target);
+ 
+         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+         el.classList.toggle('is-active');
+         $target.classList.toggle('is-active');
+ 
+       });
+     });
+   }
+ }
+ 
+ render() {
+   return (
 <div>
   <nav role="navigation" aria-label="main navigation" className="navbar-menu" id="navMenu">
             <div>
@@ -33,6 +59,7 @@ const Navbar = () => (
                     </a>
           <hr className="hr"/>
 </div>
-)
+  )}
+}
 
 export default Navbar
