5b696e96d3eeddbe47a0a9c649f3cff66ee86afc
kedorafr <alexis@kedora.fr>
2019-03-21 18:15:13 +0100

    Ajout de google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-130155672-1",
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
