2845f92ee8551f22aa3186be2a666f8096728f6c
lhirondellereviendra <45352054+lhirondellereviendra@users.noreply.github.com>
2019-06-25 22:11:20 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'https://raw.githubusercontent.com/lhirondellereviendra/lhirondellereviendra/master/static/img/lhr_logo.png',
+        //icon: 'https://raw.githubusercontent.com/lhirondellereviendra/lhirondellereviendra/master/static/img/lhr_logo.png',
       },
     },
     {
