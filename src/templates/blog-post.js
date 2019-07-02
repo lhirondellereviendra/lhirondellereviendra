9ebe9427bb525bf2ba37380d67e62eb90376e3a7
kedorafr <alexis@kedora.fr>
2019-07-02 11:11:58 +0200

    add react-share + config of disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,14 +6,22 @@ import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 import { DiscussionEmbed } from 'disqus-react'
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+  EmailShareButton,
+} from 'react-share';
 
 export const BlogPostTemplate = ({
   content,
+  urlphoto,
   contentComponent,
   description,
   tags,
   title,
   helmet,
+  slug,
 }) => {
 
   const PostContent = contentComponent || Content
@@ -31,18 +39,65 @@ export const BlogPostTemplate = ({
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
+
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
+                <h4>Catégorie(s) :</h4>
+                <ul>
                   {tags.map(tag => (
-                    <li key={tag + `tag`}>
+                    <li key={tag + `tag`} className="button is-rounded is-success" >
                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
                     </li>
                   ))}
                 </ul>
               </div>
             ) : null}
+
+            <div style={{ marginTop: `2rem` }}>
+                <h4>Partager l'article :</h4>
+                <FacebookShareButton 
+                  style={{ marginRight: `1rem`, color: `#3B5998`}} 
+                  url={`https://lhirondellereviendra.com/${slug}`} 
+                  image={urlphoto}
+                  quote={description} 
+                  className="button is-outlined is-rounded facebook-share" 
+                >
+                  <span>
+                    <i class="fab fa-facebook-square"></i>
+                  </span>
+                  <span className="text">Facebook</span>
+                </FacebookShareButton>
+                <TwitterShareButton style={{ marginRight: `1rem`,color: `#1A7DBA`}} url={`https://lhirondellereviendra.com/${slug}`} className="button is-outlined is-rounded twitter-share" >
+                  <span>
+                    <i class="fab fa-twitter"></i>
+                  </span>
+                  <span className="text">Twitter</span>
+                </TwitterShareButton>
+                <LinkedinShareButton 
+                  style={{ marginRight: `1rem`, color:`#0077B5` }} 
+                  url={`https://lhirondellereviendra.com/${slug}`} 
+                  className="button is-outlined is-rounded linkedin-share" 
+                  title={title} 
+                  image={urlphoto}
+                >
+                  <span>
+                    <i class="fab fa-linkedin"></i>
+                  </span>
+                  <span className="text">LinkedIn</span>
+                </LinkedinShareButton>
+                <EmailShareButton 
+                  style={{ marginRight: `1rem`, color:`#e5aa22` }} 
+                  url={`https://lhirondellereviendra.com/${slug}`} 
+                  className="button is-outlined is-rounded linkedin-share"
+                  subject={`Partage de l'article: ${title} L'hirondelleReviendra.com`}
+                  body={description}
+                >
+                  <span>
+                    <i class="fas fa-envelope"></i>
+                  </span>
+                  <span className="text">E-mail</span>
+                </EmailShareButton>
+            </div>
           </div>
         </div>
       </div>
@@ -59,6 +114,7 @@ BlogPostTemplate.propTypes = {
   id: PropTypes.string,
   slug: PropTypes.string,
   helmet: PropTypes.object,
+  urlphoto: PropTypes.string,
 }
 
 const BlogPost = ({ data }) => {
@@ -66,14 +122,16 @@ const BlogPost = ({ data }) => {
   const disqusShortname = 'https-lhirondellereviendra-com'
 
   const disqusConfig = {
-    url: 'www.massavo.com/',
+    url: `https://lhirondellereviendra.com${post.fields.slug}`,
     identifier: post.id,
-    title: 'Page du site fr',
+    title: post.frontmatter.title,
   };
 
   return (
     <Layout>
+      <p>{disqusConfig.url}</p>
       <BlogPostTemplate
+        urlphoto={post.frontmatter.urlphoto}
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
