29f82222bc6a906af3074cd2883412af3ba2903e
kedorafr <alexis@kedora.fr>
2019-08-02 12:55:04 +0200

    edit 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Footer from '../components/Footer'
+
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>Désolé pas page trouver</h1>
-      <p> Revenir à la page principale: <a href="https://lhirondellereviendra.com">ici</a></p>
-    </div>
+    <Header/>
+    <section className="hero is-large">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <p className="title is-1"> 🤭</p>
+          <p className="title is-2">Désolé !</p>
+          <p className="content">La page que vous recherchez n'existe pas ou a été déplacée.</p>
+        </div>
+      </div>
+    </section>
+    <Footer/>
   </Layout>
 )
 
